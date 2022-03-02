9f2e1c402dd063957b2009d5d217cbde0f9e5e9f
TFSM00 <tiagomoreira317@gmail.com>
2022-03-02 17:23:14 +0000

    Continued CSS

--- code.js
+++ code.js
@@ -1,118 +1,110 @@
-const numberButtons = document.querySelectorAll('[data-number]')
-const operatorButtons = document.querySelectorAll('[data-operator]')
-
-const equalsButton = document.getElementsByClassName('button b=')[0]
-const clearButton = document.getElementsByClassName('button clear')[0]
-const deleteButton = document.getElementsByClassName('button delete')[0]
-const pointButton = document.getElementsByClassName('button b.')[0]
-const lastCalculationScreen = document.getElementById('eq')
-const currentCalculationScreen = document.getElementById('input')
-
-console.log(equalsButton)
-// window.addEventListener('keydown', handleKeyboardInput)
-equalsButton.addEventListener('click', evaluate)
-clearButton.addEventListener('click', clearScreen)
-deleteButton.addEventListener('click', deleteNum)
-pointButton.addEventListener('click', addPoint)
-
-numberButtons.forEach((button) =>
-  button.addEventListener('click', () => addNum(button.textContent))
-);
-
-operatorButtons.forEach((button) =>
-  button.addEventListener('click', () => setOperator(button.textContent))
-);
-
-firstNum = "0"
-secondNum = "0"
-currentOperation = ""
-
-
-function clearScreen() {
-    currentCalculationScreen.textContent = "0"
-    lastCalculationScreen.textContent = ""
-    firstNum = ""
-    secondNum = ""
-    currentOperation = ""
-}
-
-function deleteNum() {
-    currentCalculationScreen.textContent = currentOperationScreen.textContent
-    .toString()
-    .slice(0, -1)
-};
-
-
-function evaluate() {
-    if (currentOperation === "") return
-    if (currentOperation === '/' && currentCalculationScreen.textContent === '0') {
-        alert("You can't divide by 0!")
-        return
-    }
-    secondNum = currentCalculationScreen.textContent
-    currentCalculationScreen.textContent = roundResult(
-      operate(currentOperation, firstNum, secondNum)
-    )
-    lastCalculationScreen.textContent = `${firstNum} ${currentOperation} ${secondNum} =`
-    currentOperation = ""
-}
-
-function roundResult(number) {
-    return Math.round(number * 1000) / 1000
-};
-
-function addPoint () {
-    if (currentCalculationScreen.textContent === '') {
-        currentCalculationScreen.textContent = '0'
-    }
-
-    if (currentCalculationScreen.textContent.includes('.')) return
-    currentCalculationScreen.textContent += '.'
-};
-
-function addNum (num) {
-    if (currentCalculationScreen.textContent === "0") {
-        currentCalculationScreen.textContent = ""
-    } else if (currentOperation != "") {
-        currentCalculationScreen.textContent = ""
-    }
-    currentCalculationScreen.textContent += num
-};
-
-
-function setOperator (op) {
-    if (currentOperation !== "") evaluate()
-    firstNum = currentCalculationScreen.textContent
-    currentOperation = op
-    lastCalculationScreen.textContent = `${firstNum} ${currentOperation}`
-}
-
-function add(a, b) {
-    return a + b;
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
 };
   
-function subtract(a, b) {
-    return a - b;
-};
-  
-function divide(a , b) {
-    return a / b
-};
-  
-function multiply(a, b) {
-    return a * b
-};
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
 
-function operate(a, op, b) {
-    if (op==="+") {
-        add(a,b);
-    } else if (op==="-") {
-        subtract(a,b);
-    } else if (op==="*") {
-        multiply(a,b);
-    } else if (op==="/" && a!= 0 && b!= 0) {
-        divide(a,b);
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
     } else {
-        return null;
+        calculator.displayValue =
+        displayValue === '0' ? digit : displayValue + digit;
     }
-};
+}
+
+function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) {
+        calculator.displayValue = '0.';
+        calculator.waitingForSecondOperand = false;
+        return;
+}
+
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+}
+
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && calculator.waitingForSecondOperand) {
+        calculator.operator = nextOperator;
+        return;
+    }
+
+    if (firstOperand == null && !isNaN(inputValue)) {
+        calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const currentValue = firstOperand || 0;
+        const result = calculate(currentValue, inputValue, operator);
+
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+}
+
+function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+        return firstOperand + secondOperand;
+    } else if (operator === '-') {
+        return firstOperand - secondOperand;
+    } else if (operator === '*') {
+        return firstOperand * secondOperand;
+    } else if (operator === '/') {
+        return firstOperand / secondOperand;
+    }
+
+    return secondOperand;
+}
+
+function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+}
+
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', event => {
+    const { target } = event;
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('operator')) {
+        handleOperator(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        resetCalculator();
+        updateDisplay();
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+});
